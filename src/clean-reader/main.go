1befaf7c6fabe68831fc7430747931bf82426fa6
Christopher Fujino <christopherfujino@gmail.com>
2026-08-28 00:10:05 -0700

    //src/clean-reader wire up url via query param

--- src/clean-reader/main.go
+++ src/clean-reader/main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"runtime/debug"
 
 	"golang.org/x/net/html"
 )
@@ -17,24 +18,22 @@ func write(w io.Writer, contents string) {
 	}
 }
 
-func main() {
-	args := os.Args[1:]
-	if len(args) != 1 {
-		panic("Usage")
-	}
-	url := args[0]
-	fmt.Printf("GET %s", url)
+func fetchPage(url string) (*bytes.Buffer, error) {
 	res, err := http.Get(url)
 	fmt.Println("")
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	if res.StatusCode < 200 || res.StatusCode >= 300 {
-		panic(fmt.Sprintf("Bad response code: %d", res.StatusCode))
+		return nil, fmt.Errorf("Bad response code: %d", res.StatusCode)
 	}
 
 	roots := parse(res.Body)
+	err = res.Body.Close()
+	if err != nil {
+		return nil, err
+	}
 	buffer := bytes.NewBuffer([]byte{})
 	write(buffer, fmt.Sprintf(`
 <!DOCTYPE html>
@@ -47,31 +46,59 @@ func main() {
 	write(buffer, `
 </html>`)
 
+	return buffer, nil
+}
+
+func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		switch r.URL.String() {
+		var handleError = func(code int, err error) {
+			var msg = err.Error()
+			http.Error(w, msg, code)
+			fmt.Fprintf(os.Stderr, "%s\n", msg)
+			debug.PrintStack()
+		}
+		var requestPath = r.URL.Path
+		switch requestPath {
 		case "/":
 			fallthrough
 		case "/index.html":
-			_, err := w.Write(buffer.Bytes())
+			queries := r.URL.Query()
+			url := queries.Get("q")
+			if url == "" {
+				handleError(404, fmt.Errorf("no query in: %s\n", r.URL))
+				return
+			}
+
+			buffer, err := fetchPage(url)
 			if err != nil {
-				panic(err)
+				handleError(404, err)
+				return
+			}
+
+			_, err = w.Write(buffer.Bytes())
+			if err != nil {
+				handleError(500, err)
+				return
 			}
 		case "/style.css":
 			w.Header().Set("Content-Type", "text/css")
 			css, err := os.Open("style.css")
 			if err != nil {
-				panic(err)
+				handleError(500, err)
+				return
 			}
 			_, err = io.Copy(w, css)
 			if err != nil {
-				panic(err)
+				handleError(500, err)
+				return
 			}
 			err = css.Close()
 			if err != nil {
-				panic(err)
+				handleError(500, err)
+				return
 			}
 		default:
-			http.Error(w, "Flooboo", 404)
+			http.Error(w, fmt.Sprintf("404: Page not found: %s", requestPath), 404)
 			fmt.Fprintf(os.Stderr, "404 %s\n", r.URL.String())
 		}
 	})
