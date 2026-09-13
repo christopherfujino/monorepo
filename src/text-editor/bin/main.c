78bb722211d412fafcef8d51a4dbfc6e3abef33e
Christopher Fujino <christopherfujino@gmail.com>
2026-09-12 17:50:33 -0700

    //src/text-editor wip

--- src/text-editor/bin/main.c
+++ src/text-editor/bin/main.c
@@ -1,10 +1,18 @@
+#include <stdlib.h> // abort()
+
 #include "editor.h"
 
-int main() {
+//static inline void ignore(void *) {}
+#define IGNORE(x) ((void *)(&x))
+
+int main(int argc, char **argv) {
+  IGNORE(argc);
+  IGNORE(argv);
   Editor e = editor_new();
 
-  if (e.buffer != nullptr) {
+  if (e.buffer == nullptr) {
     // TODO: do something interesting
+    abort();
   }
 
   return 0;
